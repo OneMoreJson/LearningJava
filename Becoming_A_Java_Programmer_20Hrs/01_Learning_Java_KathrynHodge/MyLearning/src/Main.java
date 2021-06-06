0e12c3bb6d104718d9a668a9aa63e3bd67514665
Jason Cameron <af.jasoncameron@gmail.com>
2021-06-06 13:06:38 -0500

    2021 06 06 : 0830 || LL 01 IDE
    See Read Me File

---
+++ Becoming_A_Java_Programmer_20Hrs/01_Learning_Java_KathrynHodge/MyLearning/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String question = "What is your favorite color?";
+       String choiceOne = "Red";
+       String choiceTwo = "Brown";
+       String choiceThree = "Yellow";
+
+//        Write a print statement asking the question
+//        Write a print statement giving the answer choices
+        System.out.println(question);
+        System.out.println("Choose either: " + choiceOne + ", " + choiceTwo + ", or " + choiceThree);
+
+//        Have the user input an answer
+//        Retrieve the user's input
+        Scanner getChoice = new Scanner(System.in);
+        String usersChoice = getChoice.nextLine();
+
+//        If the user's input matches the correctAnswer...
+//        Then the user is correct and we want to print out the congrats
+
+//        If the user's input does not match the correctAnswer...
+//        then the user is incorrect and we want to print out a msg that says so
+
+        if (usersChoice.equals(choiceTwo.toLowerCase())) {
+            System.out.println("Correct");
+        } else {
+            System.out.println("NOT correct");
+        }
+
+
+
+    }
+}
