05ae7e50b8b23fc2651d1029b6e35fbb4dd80a24
Jason Cameron <af.jasoncameron@gmail.com>
2021-06-07 15:33:06 -0500

    2021 06 07 : 0800 || LL 01 Functions
    See Read Me

--- Becoming_A_Java_Programmer_20Hrs/01_Learning_Java_KathrynHodge/MyLearning/src/Main.java
+++ Becoming_A_Java_Programmer_20Hrs/01_Learning_Java_KathrynHodge/MyLearning/src/Main.java
@@ -1,30 +1,8 @@
-import java.util.Scanner;
 
 public class Main {
 
-    public static void calcTotalMealPrice(double listedMealPrice,
-                                          double tipRate,
-                                          double taxRate) {
-        double tip = tipRate * listedMealPrice;
-        double tax = taxRate * listedMealPrice;
-        double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
-    }
 
     public static void main(String[] args) {
-        System.out.println("How much did was the meal?");
-        Scanner getX = new Scanner(System.in);
-        double x = getX.nextDouble();
-
-        System.out.println("What percent do you tip?");
-        Scanner getY = new Scanner(System.in);
-        double y = getY.nextDouble();
-
-        System.out.println("How much is the meal taxed?");
-        Scanner getZ = new Scanner(System.in);
-        double z = getZ.nextDouble();
-
-        calcTotalMealPrice(x, y, z);
 
     }
 }
